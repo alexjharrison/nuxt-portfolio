5d8c283f28a750cb9359c4529758eca3023bc427
Alex Harrison <alexjharrison@gmail.com>
2020-02-01 18:59:47 -0500

    update title,description,analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Portfolio | Alex Harrison',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: `Alex Harrison's Stunning Portfolio Website`
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
@@ -35,8 +35,17 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics'
   ],
+
+  /*
+   ** Google Analytics module
+   */
+  googleAnalytics: {
+    id: 'UA-130438116-2'
+  },
+
   /*
    ** Nuxt.js modules
    */
